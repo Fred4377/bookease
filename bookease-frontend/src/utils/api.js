bce295fca29616e64911a6ffbf4e7311a407eddb
Fred Otieno <obachi62@gmail.com>
2026-06-18 13:14:52 +0300

    chore: update API URL to live Render URL

--- bookease-frontend/src/utils/api.js
+++ bookease-frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://bookease-api-e81m.onrender.com/api';
 
 // Simple wrapper around fetch or Axios. We will implement fetch/axios helper.
 import axios from 'axios';
